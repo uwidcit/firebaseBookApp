19b0cfbedb08782565b46b97451295dc2acbf329
Nicholas Mendez <snickdx@gmail.com>
2024-04-03 05:25:52 +0000

    finished app

---
+++ public/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnyTSJNB9SOsx1-RPySp7OOzEOK06rM0c",
+  authDomain: "info1601-booksapp.firebaseapp.com",
+  projectId: "info1601-booksapp",
+  storageBucket: "info1601-booksapp.appspot.com",
+  messagingSenderId: "568459881258",
+  appId: "1:568459881258:web:a37d518df9686e77120c53"
+};
+
+export default firebaseConfig;
